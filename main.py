34e7418c8a4c447119b97e96a7c7331cfab01087
Lek29 <ligioner29@mail.ru>
2024-12-27 06:09:18 +0800

    офрмил точку вызова

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import requests
 from pprint import pprint
-from utils import predict_salary
-
+from terminaltables import AsciiTable
+from super_job_script import salary_in_languages_sj, print_statistics_table 
 
 
 base_url_hh = 'https://api.hh.ru/vacancies'
 
-def predict_rub_salary_hh(vacancy):
-    if not vacancy:
+def predict_salary(salary_from, salary_to):
+    if salary_from and salary_to:
+        return (salary_from + salary_to) / 2
+    elif salary_from:
+        return salary_from * 1.2
+    elif salary_to:
+        return salary_to * 0.8
+    else:
         return None
     
-    salary = vacancy.get('salary')
-    if salary and salary.get('currency') == 'RUR':
-        return predict_salary(salary.get('from'), salary.get('to'))
+
+def predict_rub_salary_hh(salary):
+    if not salary:
+        return None
     
+    if salary.get('currency') == 'RUR':
+        predicted_salary = predict_salary(salary.get('from'), salary.get('to'))
+        return predicted_salary
+    
+    return None
 
 def predict_rub_salary_sj(vacancy):
-    if vacancy.get('currency') == 'rub':
+    if vacancy.get('currency') == 'RUR':
         return predict_salary(vacancy.get('payment_from'), vacancy.get('payment_to'))
     
 
-def salary_in_languages(languages):
+def salary_in_languages_hh(languages):
     statistic = {}
 
     for language in languages:
@@ -41,6 +53,28 @@ def salary_in_languages(languages):
 
     return statistic
 
+
+def print_statistic_table_hh(statistic):
+    table_data = [
+        ['Язык программирования', 
+         'Вакансий найдено',
+         'Вакансий обработано', 
+         'Средняя зарплата'],
+    ]
+    for language, stats in statistic.items():
+        table_data.append([
+            language,
+            stats['vacancies_found'],
+            stats['vacancies_processed'],
+            stats['average_salary'],
+        ])
+    
+    table = AsciiTable(table_data)
+    table.title = 'Вакансии HH'
+
+    print(table.table)
+
+ 
 def get_all_vacancies_hh(language):
     all_salaries = []
     page = 0
@@ -60,7 +94,7 @@ def get_all_vacancies_hh(language):
         salaries_in_one_language = response.json()
 
         total_pages = salaries_in_one_language['pages']
-        print(f"Загружаем вакансии для {language}, страница {page + 1} из {total_pages}")
+        # print(f"Загружаем вакансии для {language}, страница {page + 1} из {total_pages}")
 
         for vacancy in salaries_in_one_language['items']:
             salary = vacancy.get('salary')
@@ -71,8 +105,14 @@ def get_all_vacancies_hh(language):
         page += 1
     return all_salaries, salaries_in_one_language['found']
 
+def main():
+    languages = ['Python', 'Java', 'JavaScript', 'Ruby', 'PHP', 'C++', 'C#', 'Go', 'Swift', 'TypeScript']
+    statistic_hh = salary_in_languages_hh(languages)
+    print_statistic_table_hh(statistic_hh)
 
-languages = ['Python', 'Java', 'JavaScript', 'Ruby', 'PHP', 'C++', 'C#', 'Go', 'Swift', 'TypeScript']
+    statistic_sj = salary_in_languages_sj(languages)
+    print_statistics_table(statistic_sj)
 
-pprint(salary_in_languages(languages))
 
+if __name__ == '__main__':
+    main()
